59ee4ac2a70be9b77da99d37fd11ff0f21a9e05d
lighterEB <hellohus726@126.com>
2026-06-05 17:37:20 +0800

    init spring ai assistant project

---
+++ src/main/java/github/lightereb/questionbankaiassistant/QuestionBankAiAssistantApplication.java
@@ -0,0 +1,13 @@
+package github.lightereb.questionbankaiassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBankAiAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionBankAiAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/lightereb/questionbankaiassistant/QuestionBankAiAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package github.lightereb.questionbankaiassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionBankAiAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
